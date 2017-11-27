d1062111518d1c1ad36aeec37fe0987c8e875239
Pierre LESPINASSE <pilespin@e1r9p5.42.fr>
2017-11-27 19:02:38 +0100

    Up

--- models.py
+++ models.py
@@ -11,22 +11,26 @@ class Net(nn.Module):
         self.conv1 = nn.Conv2d(1, 6, 2)
         self.conv2 = nn.Conv2d(6, 12, 2)
         self.conv3 = nn.Conv2d(12, 18, 2)
+        self.conv4 = nn.MaxPool2d(2)
         # an affine operation: y = Wx + b
-        self.fc1 = nn.Linear(450, 300)
-        self.fc2 = nn.Linear(300, 100)
-        self.fc3 = nn.Linear(100, 27)
+        # self.fc1 = nn.Linear(450, 300)
+        self.fc2 = nn.Linear(2592, 500)
+        self.fc3 = nn.Dropout(p=0.2)
+        self.fc4 = nn.Linear(500, 27)
 
 
     def forward(self, x):
 
-        x = F.max_pool2d(F.tanh(self.conv1(x)), (2, 2))
-        x = F.max_pool2d(F.tanh(self.conv2(x)), (2, 2))
+        x = F.tanh(self.conv1(x))
+        x = F.tanh(self.conv2(x))
         x = F.tanh(self.conv3(x))
+        x = F.tanh(self.conv4(x))
         # x = F.max_pool2d(F.tanh(self.conv3(x)), (2, 2))
         
-        x = self.fc1(x.view(x.size(0), -1))
+        x = self.fc2(x.view(x.size(0), -1))
         # x = self.fc1(x)
-        x = F.tanh(self.fc2(x))
+        # x = F.tanh(self.fc2(x))
         x = F.tanh(self.fc3(x))
+        x = F.tanh(self.fc4(x))
         
         return F.log_softmax(x)
