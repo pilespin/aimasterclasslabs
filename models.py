2ec2cc4fa54c5f15a5a8350aa4db6993d717df31
Pierre LESPINASSE <pilespin@e1r9p5.42.fr>
2017-11-27 18:13:01 +0100

    UP

--- models.py
+++ models.py
@@ -8,22 +8,24 @@ class Net(nn.Module):
         
         # 1 input image channel, 6 output channels, 5x5 square convolution
         # kernel
-        self.conv1 = nn.Conv2d(1, 6, 2)
+        self.conv1 = nn.Conv2d(1, 6, 4)
         self.conv2 = nn.Conv2d(6, 16, 2)
+        self.conv3 = nn.Conv2d(16, 32, 2)
         # an affine operation: y = Wx + b
-        self.fc1 = nn.Linear(576, 400)
-        self.fc2 = nn.Linear(400, 200)
-        self.fc3 = nn.Linear(200, 27)
+        self.fc1 = nn.Linear(128, 100)
+        self.fc2 = nn.Linear(100, 50)
+        self.fc3 = nn.Linear(50, 27)
 
 
     def forward(self, x):
 
         x = F.max_pool2d(F.tanh(self.conv1(x)), (2, 2))
         x = F.max_pool2d(F.tanh(self.conv2(x)), (2, 2))
+        x = F.max_pool2d(F.tanh(self.conv3(x)), (2, 2))
         
         x = self.fc1(x.view(x.size(0), -1))
         # x = self.fc1(x)
         x = F.tanh(self.fc2(x))
-        x = F.relu(self.fc3(x))
+        x = F.tanh(self.fc3(x))
         
         return F.log_softmax(x)
